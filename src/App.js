06ad4231b8cf36ac4f50d7f22a36f9c89d4dfe9f
Pramil Kesarwani <Kesarwanipramil01@gmail.com>
2021-05-17 00:05:55 +0530

    Components till Events done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import Main from "./components/Main.jsx" 
+import About from "./components/About.jsx"
+import Pricing from "./components/Pricing.jsx"
+import History from "./components/History.jsx"
+import Pricing2 from "./components/Pricing2"
+
 function App() {
   return (
     <div>
      <Main /> 
+     <About />
+     <Pricing />
+     <History />
+     <Pricing2 />
     </div>
   );
 }
