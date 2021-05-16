ac6f0a2c6543849a8667e1f8c52f4ee6a79e45ab
Pramil Kesarwani <Kesarwanipramil01@gmail.com>
2021-05-17 00:20:15 +0530

    Components till Events done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,6 +3,18 @@ import React, {Fragment} from "react";
 function Main(){
    return (
   <Fragment>
+      <div className="preloader">
+        <div className="preloader-inner">
+            <div className="preloader-icon">
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+     </div>
+     <a href="#0" className="scrollToTop"><i className="fas fa-angle-up"></i></a>
+       <div className="overlay">
+
+       </div>
        
 
     
