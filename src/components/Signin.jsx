d9788d8c6ea940eecbb9865e3f1881222cbf33f5
Aditya Ray <74045024+ADITYA-RAY@users.noreply.github.com>
2021-05-17 15:23:11 +0530

    Create Signin.jsx

---
+++ src/components/Signin.jsx
@@ -0,0 +1,46 @@
+import React, {Fragment} from "react";
+
+function Signin(){
+   return (
+  <Fragment>
+        <div className="account-section bg_img" data-background="./images/account-bg.jpg">
+        <div className="container">
+            <div className="account-title text-center">
+                <a href="index.html" className="back-home"><i className="fas fa-angle-left"></i><span>Back <span className="d-none d-sm-inline-block">To Robotix Club</span></span></a>
+                <a href="#0" className="logo">
+                    <img src="./images/logo/logo.png" alt="logo"/>
+                </a>
+            </div>
+            <div className="account-wrapper">
+                <div className="account-body">
+                    <h4 className="title mb-20">Welcome To Robotix Club</h4>
+                    <form className="account-form">
+                        <div className="form-group">
+                            <label for="sign-up">Your Email </label>
+                            <input type="text" placeholder="Enter Your Email " id="sign-up" name="email"/>
+                        </div>
+                        <div className="form-group">
+                            <label for="pass">Password</label>
+                            <input type="text" placeholder="Enter Your Password" id="pass" name="password"/>
+                            <span className="sign-in-recovery">Forgot your password? <a href="reset-password.html">recover password</a></span>
+                        </div>
+                        <div className="form-group text-center">
+                            <button type="submit" className="mt-2 mb-2">Sign In</button>
+                        </div>
+                    </form>
+                </div>
+                <div className="or">
+                    <span>OR</span>
+                </div>
+                <div className="account-header pb-0">
+                    <span className="d-block mb-30 mt-2">Sign up with your work email</span>
+                    <a href="#0" className="sign-in-with"><img src="./images/icon/google.png" alt="icon"/><span>Sign Up with Google</span></a>
+                    <span className="d-block mt-15">Don't have an account? <a href="sign-up.html">Sign Up Here</a></span>
+                </div>
+            </div>
+        </div>
+    </div>
+  </Fragment>
+   );
+}
+export default Signin;
