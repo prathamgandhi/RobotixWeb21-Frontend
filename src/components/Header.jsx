91f1b4800ffaaf85c15c572e12a3fc6163762e95
Aditya Ray <74045024+ADITYA-RAY@users.noreply.github.com>
2021-05-17 15:27:47 +0530

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, {Fragment} from "react";
+
+function Header(){
+   return (
+  <Fragment>
+           <section className="page-header single-header bg_img oh" data-background="./images/page-header.png">
+        <div className="bottom-shape d-none d-md-block">
+            <img src="./css/img/page-header.png" alt="css"/>
+        </div>
+    </section>
+  </Fragment>
+   );
+}
+export default Header;
