1a0cb7132c2da7f407e68487f1e7dd81566b43b8
Aditya Ray <74045024+ADITYA-RAY@users.noreply.github.com>
2021-05-17 15:11:06 +0530

    Create Team.jsx

---
+++ src/components/Team.jsx
@@ -0,0 +1,306 @@
+import React, {Fragment} from "react";
+
+function Team(){
+   return (
+  <Fragment>
+    <section className="team-section padding-top padding-bottom oh">
+        <div className="container">
+            <div className="section-header">
+                <h5 className="cate">Meet our most valued</h5>
+                <h2 className="title">Expert Team Members</h2>
+                <p>
+                    Our team of creative programmers,marketing experts, and members .we are to be doing what we love.
+                </p>
+            </div>
+            <ul className="nav nav-tabs feature-tab-menu">
+                <li>
+                    <a  data-toggle="tab" href="#collaboration" className="active">Core Members</a>
+                </li>
+                <li>
+                    <a  data-toggle="tab" href="#productivity">Managers</a>
+                </li>
+                <li>
+                    <a  data-toggle="tab" href="#integrations">Head Coordinator</a>
+                </li>
+            </ul>
+            <div className="tab-content">
+                <div className="tab-pane fade show active" id="collaboration">
+                    <div className="row justify-content-center mb-40-none">
+                        <div className="col-lg-4 col-md-6">
+                            <div className="team-item">
+                                <div className="team-thumb">
+                                    <img src="./images/team/team1.png" alt="team"/>
+                                </div>
+                                <div className="team-content">
+                                    <h4 className="title">
+                                        <a href="#0">Alex Love</a>
+                                    </h4>
+                                    <span className="info">Chief executive officer</span>
+                                    <a href="#0" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6">
+                            <div className="team-item">
+                                <div className="team-thumb">
+                                    <img src="./images/team/team2.png" alt="team"/>
+                                </div>
+                                <div className="team-content">
+                                    <h4 className="title">
+                                        <a href="#0">Kathy Holt</a>
+                                    </h4>
+                                    <span className="info">Growth Marketer</span>
+                                    <a href="#0" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6">
+                            <div className="team-item">
+                                <div className="team-thumb">
+                                    <img src="./images/team/team3.png" alt="team"/>
+                                </div>
+                                <div className="team-content">
+                                    <h4 className="title">
+                                        <a href="#0">Steven Mann</a>
+                                    </h4>
+                                    <span className="info">iOS Developer</span>
+                                    <a href="#0" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6">
+                            <div className="team-item">
+                                <div className="team-thumb">
+                                    <img src="./images/team/team4.png" alt="team"/>
+                                </div>
+                                <div className="team-content">
+                                    <h4 className="title">
+                                        <a href="#0">Rosa Moss</a>
+                                    </h4>
+                                    <span className="info">Chief executive officer</span>
+                                    <a href="#0" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6">
+                            <div className="team-item">
+                                <div className="team-thumb">
+                                    <img src="./images/team/team5.png" alt="team"/>
+                                </div>
+                                <div className="team-content">
+                                    <h4 className="title">
+                                        <a href="#0">Jeff Silva</a>
+                                    </h4>
+                                    <span className="info">Growth Marketer</span>
+                                    <a href="#0" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6">
+                            <div className="team-item">
+                                <div className="team-thumb">
+                                    <img src="./images/team/team6.png" alt="team"/>
+                                </div>
+                                <div className="team-content">
+                                    <h4 className="title">
+                                        <a href="#0">Sherry Parks</a>
+                                    </h4>
+                                    <span className="info">Product Designer</span>
+                                    <a href="#0" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="tab-pane fade" id="productivity">
+                    <div className="row justify-content-center mb-40-none">
+                        <div className="col-lg-4 col-md-6">
+                            <div className="team-item">
+                                <div className="team-thumb">
+                                    <img src="./images/team/team1.png" alt="team"/>
+                                </div>
+                                <div className="team-content">
+                                    <h4 className="title">
+                                        <a href="#0">Alex Love</a>
+                                    </h4>
+                                    <span className="info">Chief executive officer</span>
+                                    <a href="#0" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6">
+                            <div className="team-item">
+                                <div className="team-thumb">
+                                    <img src="./images/team/team2.png" alt="team"/>
+                                </div>
+                                <div className="team-content">
+                                    <h4 className="title">
+                                        <a href="#0">Kathy Holt</a>
+                                    </h4>
+                                    <span className="info">Growth Marketer</span>
+                                    <a href="#0" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6">
+                            <div className="team-item">
+                                <div className="team-thumb">
+                                    <img src="./images/team/team3.png" alt="team"/>
+                                </div>
+                                <div className="team-content">
+                                    <h4 className="title">
+                                        <a href="#0">Steven Mann</a>
+                                    </h4>
+                                    <span className="info">iOS Developer</span>
+                                    <a href="#0" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6">
+                            <div className="team-item">
+                                <div className="team-thumb">
+                                    <img src="./images/team/team4.png" alt="team"/>
+                                </div>
+                                <div className="team-content">
+                                    <h4 className="title">
+                                        <a href="#0">Rosa Moss</a>
+                                    </h4>
+                                    <span className="info">Chief executive officer</span>
+                                    <a href="#0" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6">
+                            <div className="team-item">
+                                <div className="team-thumb">
+                                    <img src="./images/team/team5.png" alt="team"/>
+                                </div>
+                                <div className="team-content">
+                                    <h4 className="title">
+                                        <a href="#0">Jeff Silva</a>
+                                    </h4>
+                                    <span className="info">Growth Marketer</span>
+                                    <a href="#0" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6">
+                            <div className="team-item">
+                                <div className="team-thumb">
+                                    <img src="./images/team/team6.png" alt="team"/>
+                                </div>
+                                <div className="team-content">
+                                    <h4 className="title">
+                                        <a href="#0">Sherry Parks</a>
+                                    </h4>
+                                    <span className="info">Product Designer</span>
+                                    <a href="#0" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="tab-pane fade" id="integrations">
+            <div className="row justify-content-center mb-40-none">
+                <div className="col-lg-4 col-md-6">
+                    <div className="team-item">
+                        <div className="team-thumb">
+                            <img src="./images/team/team1.png" alt="team"/>
+                        </div>
+                        <div className="team-content">
+                            <h4 className="title">
+                                <a href="#0">Alex Love</a>
+                            </h4>
+                            <span className="info">Chief executive officer</span>
+                            <a href="#0" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-4 col-md-6">
+                    <div className="team-item">
+                        <div className="team-thumb">
+                            <img src="./images/team/team2.png" alt="team"/>
+                        </div>
+                        <div className="team-content">
+                            <h4 className="title">
+                                <a href="#0">Kathy Holt</a>
+                            </h4>
+                            <span className="info">Growth Marketer</span>
+                            <a href="#0" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-4 col-md-6">
+                    <div className="team-item">
+                        <div className="team-thumb">
+                            <img src="./images/team/team3.png" alt="team"/>
+                        </div>
+                        <div className="team-content">
+                            <h4 className="title">
+                                <a href="#0">Steven Mann</a>
+                            </h4>
+                            <span className="info">iOS Developer</span>
+                            <a href="#0" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-4 col-md-6">
+                    <div className="team-item">
+                        <div className="team-thumb">
+                            <img src="./images/team/team4.png" alt="team"/>
+                        </div>
+                        <div className="team-content">
+                            <h4 className="title">
+                                <a href="#0">Rosa Moss</a>
+                            </h4>
+                            <span className="info">Chief executive officer</span>
+                            <a href="#0" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-4 col-md-6">
+                    <div className="team-item">
+                        <div className="team-thumb">
+                            <img src="./images/team/team5.png" alt="team"/>
+                        </div>
+                        <div className="team-content">
+                            <h4 className="title">
+                                <a href="#0">Jeff Silva</a>
+                            </h4>
+                            <span className="info">Growth Marketer</span>
+                            <a href="#0" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-4 col-md-6">
+                    <div className="team-item">
+                        <div className="team-thumb">
+                            <img src="./images/team/team6.png" alt="team"/>
+                        </div>
+                        <div className="team-content">
+                            <h4 className="title">
+                                <a href="#0">Sherry Parks</a>
+                            </h4>
+                            <span className="info">Product Designer</span>
+                            <a href="#0" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                </div>
+            </div>
+
+        </div>
+    </section>   
+
+    
+   
+   
+
+
+
+
+  </Fragment>
+   );
+}
+export default Team;
