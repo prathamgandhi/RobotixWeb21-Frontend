d3005ea2f332c4dab8e375abd77e9f469e568414
Jay Prakash <unijay12@gmail.com>
2021-05-20 16:06:55 +0530

    css path changed

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
+import './assets/css/bootstrap.min.css';
+import './assets/css/all.min.css';
+import './assets/css/animate.css';
+import './assets/css/nice-select.css';
+import './assets/css/owl.min.css';
+import './assets/css/magnific-popup.css';
+import './assets/css/flaticon.css';
+import './assets/css/main.css';
+
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
