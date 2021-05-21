c6ebc04e3b5ad3c13754cfa4390e1992a5d7e63d
Aditya Ray <74045024+ADITYA-RAY@users.noreply.github.com>
2021-05-21 16:31:49 +0530

    Create team.js

---
+++ public/assets/js/team.js
@@ -0,0 +1,15 @@
+    window.onload = function(){document.getElementById("core-members").style.display = "block";
+    document.getElementById("default-team-tab").className+=" team-active";};
+    function TeamDomain(evt, cityName) {
+      var i, tabcontent, tablinks;
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+      tablinks = document.getElementsByClassName("tablinks");
+      for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" team-active", "");
+      }
+      document.getElementById(cityName).style.display = "block";
+      evt.currentTarget.className += " team-active";
+    }
